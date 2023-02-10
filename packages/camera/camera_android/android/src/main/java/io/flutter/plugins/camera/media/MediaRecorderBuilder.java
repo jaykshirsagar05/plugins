6495a6b55053c3160e29fb3097d62b68f07da305
jaykshirsagar <jaykshirsagar502@gmail.com>
2023-02-10 15:07:03 -0500

    lowframerate

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/media/MediaRecorderBuilder.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/media/MediaRecorderBuilder.java
@@ -87,8 +87,8 @@ public class MediaRecorderBuilder {
       }
       mediaRecorder.setVideoEncoder(videoProfile.getCodec());
       mediaRecorder.setVideoEncodingBitRate(videoProfile.getBitrate());
-      mediaRecorder.setVideoFrameRate(60);
-      mediaRecorder.setCaptureRate(60);
+      // mediaRecorder.setVideoFrameRate(60);
+      mediaRecorder.setCaptureRate(10);
       mediaRecorder.setVideoSize(videoProfile.getWidth(), videoProfile.getHeight());
       mediaRecorder.setVideoSize(videoProfile.getWidth(), videoProfile.getHeight());
     } else {
@@ -100,8 +100,8 @@ public class MediaRecorderBuilder {
       }
       mediaRecorder.setVideoEncoder(camcorderProfile.videoCodec);
       mediaRecorder.setVideoEncodingBitRate(camcorderProfile.videoBitRate);
-      mediaRecorder.setVideoFrameRate(60);
-      mediaRecorder.setCaptureRate(60);
+      // mediaRecorder.setVideoFrameRate(60);
+      mediaRecorder.setCaptureRate(10);
       mediaRecorder.setVideoSize(
           camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
     }
